aac19b8cdc850e4be71183b4e0ec90df2b8cc388
Jaya DeHart <dehartj97@gmail.com>
2022-06-23 15:19:39 -0700

    working namespaces

--- index.js
+++ index.js
@@ -11,24 +11,35 @@ const io = require('socket.io')(server, {
 
 const assignRole = require('./util/assignRole');
 
-const dns = io.of(/namespace-.+/);
+const mainLobby = io.of('/initial');
 
-dns.on('connection', (socket) => {
-  let players = [];
-  console.log(socket.id);
-  console.log(socket.nsp);
+mainLobby.on('connection', (socket) => {
+  socket.emit('welcome', { message: 'welcome to the main lobby!' });
+});
+
+let players = {};
+
+io.of(/\/namespace-.+/).on('connection', (socket) => {
+  insp = socket.nsp;
+  inspName = socket.nsp.name;
+  if (!players.hasOwnProperty(inspName)) {
+    players[inspName] = [];
+  }
+  console.log(`welcome ${socket.id} to ${inspName}`);
   socket.on('board-state-update', (action) => {
-    console.log(action);
-    dns.emit('server-state-update', action);
+    socket.broadcast.emit('server-state-update', action);
   });
   socket.on('join', (payload) => {
-    players.push(payload);
-    dns.emit('join', payload);
+    players[inspName].push({
+      id: socket.id,
+      name: payload,
+    });
+    console.log(players);
+    insp.emit('players', players[inspName]);
+    //there should be no pushing on the client side. the client will simply accept the server side players array as their own. This will help avoid duplicates
   });
   socket.on('start', () => {
-    const roles = assignRole(players);
-    console.log(roles);
-    io.emit('setRoles', roles);
+    //assign and send roles
   });
 });
 
